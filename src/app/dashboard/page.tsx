6db9b1f5df6249376fb7e9bc0d83126a7c20882d
Michelle Tan <dev.michelletan@gmail.com>
2024-03-08 22:49:06 +0700

    set sidebar, mobile sidebar, and navbar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import {
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-} from "@heroicons/react/24/outline";
 import { Navbar } from "./_components/navbar";
-import { Sidebar } from "./_components/sidebar";
-
-// const navigation = [
-//   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-//   { name: "Team", href: "#", icon: UsersIcon, current: false },
-//   { name: "Projects", href: "#", icon: FolderIcon, current: false },
-//   { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-//   { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-//   { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
-// ];
-
-// const sidebarProps = {
-//   navigation,
-// };
 
 export default async function DashboardLayout() {
   const session = await getServerSession(authOptions);
@@ -30,7 +8,6 @@ export default async function DashboardLayout() {
 
   return (
     <div>
-      <Sidebar  />
       <Navbar user={user} />
       <div>
         <div className="lg:pl-72">
