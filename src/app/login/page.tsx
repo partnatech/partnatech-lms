6d8e41ddd191bee03de5e62a85481a3478c53e85
Michelle Tan <dev.michelletan@gmail.com>
2024-03-12 21:46:25 +0700

    init oauth login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,20 +6,25 @@ import { Logo } from "@/components/logo";
 export default function LoginPage() {
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <Logo />
-          <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Log in to your account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-          <div className="bg-white p-6 shadow sm:rounded-lg sm:px-12">
+      <div className="flex min-h-full flex-1">
+        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <Logo />
+            <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to Your Account
+            </h2>
+          </div>
           <Suspense>
             <LoginForm />
           </Suspense>
-          </div>
+        </div>
+        <div className="relative hidden w-0 flex-1 lg:block">
+          <Image
+            src={"/images/auth.jpg"}
+            alt="Partnatech"
+            layout="fill"
+            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+          />
         </div>
       </div>
     </>
--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -2,7 +2,11 @@
 import DarkModeToggle from "@/components/dark-mode-toggle";
 import { cn } from "@/utils/cn";
 import { Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+  Bars3Icon,
+  BellIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 import { ChevronDoubleDownIcon } from "@heroicons/react/24/solid";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
@@ -20,9 +24,22 @@ interface NavbarProps {
 }
 
 export const NavbarRoutes = ({ user, setSidebarOpen }: NavbarProps) => {
+  const getFirstWordBeforeSpace = (str: string) => {
+    if (!str) return ""; // Handle empty string
+    const firstSpaceIndex = str.indexOf(" ");
+    if (firstSpaceIndex === -1) return str; // Return the whole string if there's no space
+    return str.slice(0, firstSpaceIndex);
+  };
+
+  const firstWord = getFirstWordBeforeSpace(user?.name || "");
+
   return (
     <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 dark:border-gray-900 bg-gray-50 dark:bg-gray-950 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-      <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
+      <button
+        type="button"
+        className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+        onClick={() => setSidebarOpen(true)}
+      >
         <span className="sr-only">Open sidebar</span>
         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
       </button>
@@ -31,49 +48,53 @@ export const NavbarRoutes = ({ user, setSidebarOpen }: NavbarProps) => {
       <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
 
       <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-        {/* <form className="relative flex flex-1" action="#" method="GET">
-          <label htmlFor="search-field" className="sr-only">
-            Search
-          </label>
-          <MagnifyingGlassIcon
-            className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          <input
-            id="search-field"
-            className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-            placeholder="Search..."
-            type="search"
-            name="search"
-          />
-        </form> */}
         <div className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm" />
         <div className="flex items-center gap-x-4 lg:gap-x-6">
           <DarkModeToggle />
-          <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+          >
             <span className="sr-only">View notifications</span>
             <BellIcon className="h-6 w-6" aria-hidden="true" />
           </button>
 
           {/* Separator */}
-          <div className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true" />
+          <div
+            className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
+            aria-hidden="true"
+          />
 
           {/* Profile dropdown */}
           <Menu as="div" className="relative">
             <Menu.Button className="-m-1.5 flex items-center p-1.5">
               <span className="sr-only">Open user menu</span>
               {user?.image ? (
-                <Image className="h-8 w-8 rounded-full bg-gray-50" src={user?.image} alt={user?.name || "user"} />
+                <Image
+                  className="h-8 w-8 rounded-full bg-gray-50"
+                  src={user?.image}
+                  alt={user?.name || "user"}
+                  width={100}
+                  height={100}
+                />
               ) : (
                 <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full">
-                  <span className="font-medium text-gray-600">{user?.name?.charAt(0).toUpperCase()}</span>
+                  <span className="font-medium text-gray-600">
+                    {user?.name?.charAt(0).toUpperCase()}
+                  </span>
                 </div>
               )}
               <span className="hidden lg:flex lg:items-center">
-                <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
-                  {user?.name}
+                <span
+                  className="ml-4 text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+                  aria-hidden="true"
+                >
+                  {firstWord}
                 </span>
-                <ChevronDoubleDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                <ChevronDoubleDownIcon
+                  className="ml-2 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
               </span>
             </Menu.Button>
             <Transition
@@ -90,7 +111,10 @@ export const NavbarRoutes = ({ user, setSidebarOpen }: NavbarProps) => {
                   {({ active }) => (
                     <a
                       href="#"
-                      className={cn(active ? "bg-gray-50" : "", "block px-3 py-1 text-sm leading-6 text-gray-900")}
+                      className={cn(
+                        active ? "bg-gray-50" : "",
+                        "block px-3 py-1 text-sm leading-6 text-gray-900"
+                      )}
                     >
                       Profile
                     </a>
@@ -100,7 +124,10 @@ export const NavbarRoutes = ({ user, setSidebarOpen }: NavbarProps) => {
                   {({ active }) => (
                     <a
                       href="#"
-                      className={cn(active ? "bg-gray-50" : "", "block px-3 py-1 text-sm leading-6 text-gray-900")}
+                      className={cn(
+                        active ? "bg-gray-50" : "",
+                        "block px-3 py-1 text-sm leading-6 text-gray-900"
+                      )}
                       onClick={() => signOut()}
                     >
                       Logout
