33308868d21520201200fc28c23d11552e2d5de6
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2024-04-16 16:21:32 +0700

    feat: create categories page

---
+++ src/app/dashboard/my-learning/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const MyLearningPage = () => {
+  return (
+    <div>
+      <p className="text-sm text-typography-secondary">
+        Guiding you through structured pathways for achieving your expert goals efficiently.
+      </p>
+    </div>
+  )
+}
+
+export default MyLearningPage
