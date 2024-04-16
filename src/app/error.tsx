9d93d0f910ddc2d2d6712d234bb8b4e565c91f83
Naufaldi <naufaldi.rafif@gmail.com>
2024-04-16 14:40:55 +0700

    feat: add error and not found (#21)
    Co-authored-by: Naufaldi Rafif S <naufaldi.rafif@efishery.com>

---
+++ src/app/error.tsx
@@ -0,0 +1,33 @@
+'use client'; // Error components must be Client Components
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="mx-auto flex w-full max-w-7xl flex-auto flex-col justify-center px-6 py-24 sm:py-64 lg:px-8">
+      <p className="text-base font-semibold leading-8 text-indigo-600">404</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        Page not found
+      </h1>
+      <p className="mt-6 text-base leading-7 text-gray-600">
+        Sorry, we couldn’t find the page you’re looking for.
+      </p>
+      <div className="mt-10">
+        <a href="#" className="text-sm font-semibold leading-7 text-indigo-600">
+          <span aria-hidden="true">&larr;</span> Back to home
+        </a>
+      </div>
+    </div>
+  );
+}
