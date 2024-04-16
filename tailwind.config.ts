26e5822dd1cf281f418964e6b68d315502bea07a
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2024-04-16 16:15:07 +0700

    style: udpate tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   darkMode: "class",
@@ -9,13 +9,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#2DD4BF",
+        secondary: "#A3E635",
+        background: {
+          primary: "#18181B",
+          secondary: "#1D1D20",
+          tertiary: "#242427",
+        },
+        typography: {
+          primary: "#FFFFFF",
+          secondary: "#A1A1AA",
+        },
+        line: {
+          primary: "#302F31",
+          secondary: "#242427",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
-export default config;
+}
+export default config
