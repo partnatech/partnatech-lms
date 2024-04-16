e3dd283649151585116b1eef2432182dfdc895e1
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2024-04-16 16:13:21 +0700

    feat: create strapi type

---
+++ src/types/strapi/course.ts
@@ -0,0 +1,56 @@
+import { DataWrapper, Image } from "."
+import { Category } from "./category"
+
+export type Difficulty = "beginner" | "intermediate" | "advance"
+export type CourseResponse = DataWrapper<Course>
+
+export interface CourseContentItem {
+  title: string
+  video_link: string
+  type: string
+  slug: string
+  duration: number
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+}
+
+export interface CourseSection {
+  title: string
+  slug: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  course_content_items: {
+    data: DataWrapper<CourseContentItem>[]
+  }
+}
+
+export interface Course {
+  title: string
+  slug: string
+  descriptions: string
+  preview_video_link: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  short_descriptions: string
+  total_duration: number
+  total_lesson: number
+  cover_images: Image
+  difficulty: Difficulty
+  list_of_learning: string[]
+  tools_used: {
+    id: number
+    title: string
+    link: string
+    description: string
+    icons: Image
+  }[]
+  category: {
+    data: DataWrapper<Category>
+  }
+  course_sections: {
+    data: DataWrapper<CourseSection>[]
+  }
+}
