80f4a33179f921ce54e0113d3ae4c1ffe1e85c37
dianyehezkiel <dianyehezkiel@gmail.com>
2024-04-17 17:51:23 +0700

    feat: add constants

---
+++ src/lib/constants/bootcamp-detail-sample.ts
@@ -0,0 +1,64 @@
+
+export const bootcamp = {
+  title: 'Bootcamp Data Science dan Data Analyst for Beginner',
+  description: 'Learn the basics of Data Analysis & key foundational concepts with this online intro Learning Path.',
+  period: '11 Mar - 11 Apr 2024',
+  location: 'Online',
+  level: 'Beginner',
+  about: 'Data Analyst menjadi profesi dengan urutan pertama, yang paling dibutuhkan pada tahun 2030 menurut World Economic Forum. Bersiap kembangkan karir melalui pelatihan online secara intensif dan live di Bootcamp Data Analysis. Belajar bersama mentor expert dari Top Companies, dimulai dari level dasar hingga lanjut.',
+  benefits: [
+    '17x sesi live class bersama mentor experts dari top companies',
+    'Tutor berpengalaman dengan level Lead/Manager/Head',
+    'Latihan dan praktik di tiap sesi',
+    'Sesi sharing pengalaman bersama praktisi',
+    'Real Portfolio Project',
+    'Mendapat rekaman video setiap Sesi untuk dipelajari Kembali.',
+    'Mendapatkan e-Certificate selesai pelatihan.',
+    'Special award untuk tim terbaik.',
+  ],
+  mentors: [
+    {
+      name: 'Rahadian Rizky',
+      description: 'Data Engineer Lead - Telkom',
+      image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    },
+    {
+      name: 'Timothy Ronald',
+      description: 'Head of Data - Tokopedia',
+      image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    },
+  ],
+  scheduleDetail: {
+    period: '11 Mar - 11 Apr 2024',
+    day: 'Senin, Rabu, Jumat',
+    time: '19.00 - 21.00 WIB',
+    location: 'Online via Zoom',
+    discussionPlatform: 'Discord',
+  },
+  curriculums: [
+    {
+      phaseTitle: 'Preparation',
+      phasePeriod: '11 March 2024',
+      phaseDescription: 'Di fase persiapan, siswa akan belajar Data Analyst dari mulai mengumpulkan informasi dan menarik kesimpulan dari data, serta visualisasi data untuk membuat dashboard yang interaktif.',
+      materialTitle: 'Fundamental Python',
+      materialDescription: 'Belajar Python, SQL, Statistik, serta Data Visualization. Siswa akan mempresentasikan hasil data analytics dengan business knowledge dari dashboard yang dibuat.',
+      tools: ['python', 'github', 'seaborn', 'tableau', 'pandas', 'numpy', 'postgresql'],
+    },
+    {
+      phaseTitle: 'Bootcamp Phase - 1',
+      phasePeriod: '12 March 2024 - 19 March 2024 ',
+      phaseDescription: 'Pada fase 1, siswa akan mempelajari cara membuat Machine Learning Supervised dan Unsupervised Learning dengan Feature Engineering, Model Evaluation, dan Improvement, serta Deployment model yang telah dibuat.',
+      materialTitle: 'Deployment Machine Learning',
+      materialDescription: 'Membuat Machine Learning model dengan Feature Engineering, Model Evaluation, dan Improvement, serta Deployment model yang telah dibuat.',
+      tools: ['scikit', 'scipy', 'tensorflow', 'streamlit', 'hugging-face'],
+    },
+    {
+      phaseTitle: 'Bootcamp Phase - 2',
+      phasePeriod: '23 March 2024 - 30 March 2024 ',
+      phaseDescription: 'Pada fase 2, siswa akan mempelajari cara menganalisis Big Data dan mengerjakan projek dari studi kasus nyata. Siswa akan mendapat sertifikat setelah berhasil menyelesaikan projek.',
+      materialTitle: 'Big Data Analytics',
+      materialDescription: 'Mempelajari Big Data Analytics menggunakan Amazon Web Service hingga membuat final projek yang telah ditentukan Hiring Partner.',
+      tools: ['docker', 'elasticsearch', 'great-expectations', 'airflow', 'aws', 'spark'],
+    },
+  ]
+}
