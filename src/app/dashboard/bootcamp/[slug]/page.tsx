e412ab367366854339327bc7ab46b8ff6d049766
dianyehezkiel <dianyehezkiel@gmail.com>
2024-04-17 17:52:21 +0700

    feat: add accordion and use bootcamp constant

--- src/app/dashboard/bootcamp/[slug]/page.tsx
+++ src/app/dashboard/bootcamp/[slug]/page.tsx
@@ -5,44 +5,8 @@ import { BootcampDetailHeader } from "../_components/bootcamp-header";
 import { MentorCard } from "../_components/mentor-card";
 import { ContentContainer } from "../_components/content-container";
 import { ScheduleCards } from "../_components/schedule-cards";
-
-const bootcamp = {
-  title: 'Bootcamp Data Science dan Data Analyst for Beginner',
-  description: 'Learn the basics of Data Analysis & key foundational concepts with this online intro Learning Path.',
-  period: '11 Mar - 11 Apr 2024',
-  location: 'Online',
-  level: 'Beginner',
-  about: 'Data Analyst menjadi profesi dengan urutan pertama, yang paling dibutuhkan pada tahun 2030 menurut World Economic Forum. Bersiap kembangkan karir melalui pelatihan online secara intensif dan live di Bootcamp Data Analysis. Belajar bersama mentor expert dari Top Companies, dimulai dari level dasar hingga lanjut.',
-  benefits: [
-    '17x sesi live class bersama mentor experts dari top companies',
-    'Tutor berpengalaman dengan level Lead/Manager/Head',
-    'Latihan dan praktik di tiap sesi',
-    'Sesi sharing pengalaman bersama praktisi',
-    'Real Portfolio Project',
-    'Mendapat rekaman video setiap Sesi untuk dipelajari Kembali.',
-    'Mendapatkan e-Certificate selesai pelatihan.',
-    'Special award untuk tim terbaik.',
-  ],
-  mentors: [
-    {
-      name: 'Rahadian Rizky',
-      description: 'Data Engineer Lead - Telkom',
-      image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-    },
-    {
-      name: 'Timothy Ronald',
-      description: 'Head of Data - Tokopedia',
-      image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-    },
-  ],
-  scheduleDetail: {
-    period: '11 Mar - 11 Apr 2024',
-    day: 'Senin, Rabu, Jumat',
-    time: '19.00 - 21.00 WIB',
-    location: 'Online via Zoom',
-    discussionPlatform: 'Discord',
-  },
-}
+import { CurriculumAccordion } from "../_components/curriculum-accordion";
+import { bootcamp } from "@/lib/constants/bootcamp-detail-sample";
 
 const BootcampDetailPage = ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
@@ -101,6 +65,14 @@ const BootcampDetailPage = ({ params }: { params: { slug: string } }) => {
         <ContentContainer id="schedule" title="Schedule, Location & Group">
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
             <ScheduleCards {...bootcamp.scheduleDetail} />
+          </div>
+        </ContentContainer>
+        {/* Curriculum */}
+        <ContentContainer id="curriculum" title="Curriculum">
+          <div className="space-y-6">
+            {bootcamp.curriculums.map((curriculum, idx) => (
+              <CurriculumAccordion key={`curriculum-${idx}`} {...curriculum} />
+            ))}
           </div>
         </ContentContainer>
       </div>
